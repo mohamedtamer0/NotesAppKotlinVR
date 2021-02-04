68b32a0095a6b6b991c59fbf425d72ad215dbcec
mohamed tamer0 <mt4452242@gmail.com>
2021-02-04 02:14:02 +0200

    Update Add Image

--- app/src/main/java/com/example/notesappkotlinvr/CreateNoteFragment.kt
+++ app/src/main/java/com/example/notesappkotlinvr/CreateNoteFragment.kt
@@ -34,6 +34,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
+
     var selectedColor = "#171C26"
     var currentDate:String? = null
     private var READ_STORAGE_PERM = 123
@@ -41,6 +42,7 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
     private var selectedImagePath = ""
     private var webLink = ""
     private var noteId = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -48,8 +50,12 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_note, container, false)
     }
@@ -59,13 +65,14 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
         fun newInstance() =
                 CreateNoteFragment().apply {
                     arguments = Bundle().apply {
-
                     }
                 }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
         if (noteId != -1){
 
             launch {
@@ -111,7 +118,7 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
         currentDate = sdf.format(Date())
         colorView.setBackgroundColor(Color.parseColor(selectedColor))
 
-        tvDateTime.text =currentDate
+        tvDateTime.text = currentDate
 
         imgDone.setOnClickListener {
             if (noteId != -1){
@@ -125,7 +132,9 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
             requireActivity().supportFragmentManager.popBackStack()
         }
 
-        imgMore.setOnClickListener {
+        imgMore.setOnClickListener{
+
+
             var noteBottomSheetFragment = NoteBottomSheetFragment.newInstance(noteId)
             noteBottomSheetFragment.show(requireActivity().supportFragmentManager,"Note Bottom Sheet Fragment")
         }
@@ -133,23 +142,32 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
         imgDelete.setOnClickListener {
             selectedImagePath = ""
             layoutImage.visibility = View.GONE
+
         }
 
         btnOk.setOnClickListener {
-            if (etWebLink.text.toString().trim().isNotEmpty()) {
+            if (etWebLink.text.toString().trim().isNotEmpty()){
                 checkWebUrl()
-            } else {
-                Toast.makeText(context, "Url Is Not Required",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(requireContext(),"Url is Required",Toast.LENGTH_SHORT).show()
             }
         }
 
         btnCancel.setOnClickListener {
-            if (noteId != -1) {
+            if (noteId != -1){
                 tvWebLink.visibility = View.VISIBLE
                 layoutWebUrl.visibility = View.GONE
-            }else {
+            }else{
                 layoutWebUrl.visibility = View.GONE
             }
+
+        }
+
+        imgUrlDelete.setOnClickListener {
+            webLink = ""
+            tvWebLink.visibility = View.GONE
+            imgUrlDelete.visibility = View.GONE
+            layoutWebUrl.visibility = View.GONE
         }
 
         tvWebLink.setOnClickListener {
@@ -185,21 +203,20 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
             }
         }
     }
-
-
-    private fun saveNote() {
+    private fun saveNote(){
 
         if (etNoteTitle.text.isNullOrEmpty()){
-            Toast.makeText(context, "Note Title is Required",Toast.LENGTH_SHORT).show()
+            Toast.makeText(context,"Note Title is Required",Toast.LENGTH_SHORT).show()
+        }
+        else if (etNoteSubTitle.text.isNullOrEmpty()){
+
+            Toast.makeText(context,"Note Sub Title is Required",Toast.LENGTH_SHORT).show()
         }
 
-       else if (etNoteSubTitle.text.isNullOrEmpty()){
-            Toast.makeText(context, "Note Sub Title is Required",Toast.LENGTH_SHORT).show()
-       }
+        else if (etNoteDesc.text.isNullOrEmpty()){
 
-       else if (etNoteDesc.text.isNullOrEmpty()){
-            Toast.makeText(context, "Note Description is Required",Toast.LENGTH_SHORT).show()
-       }
+            Toast.makeText(context,"Note Description is Required",Toast.LENGTH_SHORT).show()
+        }
 
         else{
 
@@ -225,24 +242,32 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
             }
         }
 
-
     }
 
-    private fun checkWebUrl() {
-        if(Patterns.WEB_URL.matcher(etWebLink.text.toString()).matches()){
+    private fun deleteNote(){
+
+        launch {
+            context?.let {
+                NotesDatabase.getDatabase(it).noteDao().deleteSpecificNote(noteId)
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+        }
+    }
+
+    private fun checkWebUrl(){
+        if (Patterns.WEB_URL.matcher(etWebLink.text.toString()).matches()){
             layoutWebUrl.visibility = View.GONE
             etWebLink.isEnabled = false
             webLink = etWebLink.text.toString()
             tvWebLink.visibility = View.VISIBLE
             tvWebLink.text = etWebLink.text.toString()
-        } else {
-            Toast.makeText(requireContext(), "Url Is Not Valid",Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(requireContext(),"Url is not valid",Toast.LENGTH_SHORT).show()
         }
     }
 
 
-
-    private val BroadcastReceiver : BroadcastReceiver = object : BroadcastReceiver(){
+    private val BroadcastReceiver : BroadcastReceiver = object :BroadcastReceiver(){
         override fun onReceive(p0: Context?, p1: Intent?) {
 
             var actionColor = p1!!.getStringExtra("action")
@@ -289,13 +314,17 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
 
                 }
 
-                "Image" -> {
+                "Image" ->{
                     readStorageTask()
                     layoutWebUrl.visibility = View.GONE
                 }
 
-                "WebUrl" -> {
+                "WebUrl" ->{
                     layoutWebUrl.visibility = View.VISIBLE
+                }
+                "DeleteNote" -> {
+                    //delete note
+                    deleteNote()
                 }
 
 
@@ -312,40 +341,40 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
 
     }
 
-
     override fun onDestroy() {
+
         LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(BroadcastReceiver)
         super.onDestroy()
+
+
     }
 
-
-    private fun hasReadStoragePerm() : Boolean {
+    private fun hasReadStoragePerm():Boolean{
         return EasyPermissions.hasPermissions(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE)
     }
 
 
-    private fun readStorageTask() {
-        if (hasReadStoragePerm()) {
+    private fun readStorageTask(){
+        if (hasReadStoragePerm()){
+
 
             pickImageFromGallery()
-        }else {
+        }else{
             EasyPermissions.requestPermissions(
-                requireActivity(),
-                getString(R.string.storage_permission_text),
-                READ_STORAGE_PERM,
-                Manifest.permission.READ_EXTERNAL_STORAGE
+                    requireActivity(),
+                    getString(R.string.storage_permission_text),
+                    READ_STORAGE_PERM,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
             )
         }
     }
 
-
-    private fun pickImageFromGallery() {
+    private fun pickImageFromGallery(){
         var intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+        if (intent.resolveActivity(requireActivity().packageManager) != null){
             startActivityForResult(intent,REQUEST_CODE_IMAGE)
         }
     }
-
 
     private fun getPathFromUri(contentUri: Uri): String? {
         var filePath:String? = null
@@ -387,9 +416,9 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
 
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
@@ -397,25 +426,23 @@ class CreateNoteFragment : BaseFragment(), EasyPermissions.PermissionCallbacks, 
     }
 
 
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(),perms)) {
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(),perms)){
             AppSettingsDialog.Builder(requireActivity()).build().show()
         }
     }
 
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        TODO("Not yet implemented")
-    }
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
 
-    override fun onRationaleAccepted(requestCode: Int) {
-        TODO("Not yet implemented")
     }
 
     override fun onRationaleDenied(requestCode: Int) {
-        TODO("Not yet implemented")
+
     }
 
+    override fun onRationaleAccepted(requestCode: Int) {
+
+    }
 
 }
 
